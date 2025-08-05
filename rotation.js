fb12492a47d68630b4c8cbcdcc95a4d5259d9a1f
Collinsk70 <collinskarweru@gmail.com>
2025-08-05 14:19:04 +0300

    commit

--- rotation.js
+++ rotation.js
@@ -1,12 +1,10 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function solution(A, K) {
- const N = A.length;
-    if (N === 0) return A; // Empty array remains the same
-
-    K = K % N;
-    if (K === 0) return A; // No rotation needed
-
-    return A.slice(-K).concat(A.slice(0, N - K));
-}
+function solution(A) {
+let unpaired = 0;
+    for (let i = 0; i < A.length; i++) {
+        unpaired ^= A[i]; // XOR each element
+    }
+    return unpaired;
+    }
