390446fe2d820ca9bb9668577998dc6c01a6afe3
Collinsk70 <collinskarweru@gmail.com>
2025-08-07 16:13:48 +0300

    commit

--- rotation.js
+++ rotation.js
@@ -1,7 +1,11 @@
-function solution (X,Y,D){
-    const distance = Y - X;
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
 
-    jump = Math.ceil(distance/D)
+function solution(A) {
+    // Implement your solution here
+    const N = A.length;
+    const expectedSum = (N + 1) * (N + 2) / 2;
+    const actualSum = A.reduce((acc, num) => acc + num, 0);
+    return expectedSum - actualSum;
 
-    return jump
 }
